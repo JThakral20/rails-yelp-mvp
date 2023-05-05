d8f6fa4d3866f164df69326031527521d296af49
Jasmine Thakral <jasminethakral@gmail.com>
2023-05-05 14:25:47 +0100

    second challenge

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: "restaurants#index"
+  resources :restaurants, only: [:index, :show, :new, :create] do
+  resources :reviews, only: [:create]
+  # get "restaurants/new", to: "restaurants#new"
+  # post "restaurants", to: "restaurants#create"
+end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = {name: "Dishoom", address: "7 Boundary St, London E2 7JE", category:"chinese"}
+pizza_east =  {name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category:"chinese"}
+jasmine =  {name: "Jasmine", address: "80A Rose Street, London J2 8PQ",category: "italian"}
+pizza_express =  {name: "Pizza Express", address: "SU44-45, Priory Meadow Shopping Centre, Hastings TN34 1PH", category:"italian"}
+smith_something = {name: "Smith Something", address: "SU44-45, Priory Road, Hastings TN34 1PH", category:"italian"}
+
+
+[dishoom, pizza_east,jasmine,pizza_express,smith_something].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
